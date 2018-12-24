06a4f3143a894312a6a81b1ab1e3bfb6a5a747b7
onemen <tabmix.onemen@gmail.com>
2018-12-24 22:16:16 +0200

    Use original Prism with  prism-react-renderer

--- packages/web/components/CodeFile.tsx
+++ packages/web/components/CodeFile.tsx
@@ -1,4 +1,5 @@
-//import * as Prism from "prismjs";
+import { useRef, useState, useEffect } from "react";
+import * as Prism from "prismjs";
 //import "prismjs/themes/prism.css";
 import "prismjs/themes/prism-coy.css";
 
@@ -11,6 +12,7 @@ import {
   FindCodeReviewQuestionsQuery,
 } from "./apollo-components";
 import { filenameToLang } from "../utils/filenameToLang";
+import { loadLanguage } from "../utils/loadLanguage";
 import { QuestionSection } from "./QuestionSection";
 
 interface Props {
@@ -35,7 +37,82 @@ const SelectLines = (prop: FindCodeReviewQuestionsQuery) => {
   `;
 };
 
+interface HighlightProps {
+  code: string | null;
+  lang: Language;
+  data: FindCodeReviewQuestionsQuery;
+}
+
+const HighlightCode: React.SFC<HighlightProps> = ({ code, lang, data }) => {
+  const hasLoadedLanguage = useRef(false);
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    if (!hasLoadedLanguage.current) {
+      loadLanguage(lang)
+        .then(() => {
+          setloading(false);
+          hasLoadedLanguage.current = true;
+        })
+        .catch(() => {});
+    }
+  }, []);
+
+  if (loading) {
+    return null;
+  }
+
+  const Pre = styled.pre`
+    text-align: left;
+    margin: 4em 0;
+    padding: 0.5em;
+
+    & .token-line {
+      line-height: 1.3em;
+      height: 1.3em;
+    }
+
+    & .token-line:nth-child(odd) {
+      background: #f3faff;
+    }
+
+    ${SelectLines(data)};
+  `;
+
+  const LineNo = styled.span`
+    display: inline-block;
+    width: 2em;
+    user-select: none;
+    opacity: 0.3;
+  `;
+
+  // Use original Prism.
+  // the version included in prism-react-renderer doesn't load all languages
+  const props = {
+    ...defaultProps,
+    Prism,
+  };
+
+  return (
+    <Highlight {...props} theme={undefined} code={code} language={lang}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <Pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              <LineNo>{i + 1}</LineNo>
+              {line.map((token, key) => {
+                return <span {...getTokenProps({ token, key })} />;
+              })}
+            </div>
+          ))}
+        </Pre>
+      )}
+    </Highlight>
+  );
+};
+
 export const CodeFile: React.SFC<Props> = ({ code, path, postId }) => {
+  // const lang: Language = path ? filenameToLang(path) : "";
   const lang: Language = path ? filenameToLang(path) : "";
   const variables = {
     path,
@@ -49,53 +126,9 @@ export const CodeFile: React.SFC<Props> = ({ code, path, postId }) => {
           return null;
         }
 
-        const Pre = styled.pre`
-          text-align: left;
-          margin: 4em 0;
-          padding: 0.5em;
-
-          & .token-line {
-            line-height: 1.3em;
-            height: 1.3em;
-          }
-
-          & .token-line:nth-child(odd) {
-            background: #f3faff;
-          }
-
-          ${SelectLines(data)};
-        `;
-
-        const LineNo = styled.span`
-          display: inline-block;
-          width: 2em;
-          user-select: none;
-          opacity: 0.3;
-        `;
-
         return (
           <>
-            <Highlight
-              {...defaultProps}
-              theme={undefined}
-              code={code}
-              language={lang}
-            >
-              {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                <Pre className={className} style={style}>
-                  {tokens.map((line, i) => {
-                    return (
-                      <div {...getLineProps({ line, key: i })}>
-                        <LineNo>{i + 1}</LineNo>
-                        {line.map((token, key) => {
-                          return <span {...getTokenProps({ token, key })} />;
-                        })}
-                      </div>
-                    );
-                  })}
-                </Pre>
-              )}
-            </Highlight>
+            <HighlightCode code={code} lang={lang} data={data} />
             <QuestionSection
               variables={variables}
               code={code || ""}
