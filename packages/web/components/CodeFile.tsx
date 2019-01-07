cb04b4743fd9bcd3cbed535d5e0f6338a60b6ed0
onemen <tabmix.onemen@gmail.com>
2019-01-07 14:25:30 +0200

    Wip, not working

--- packages/web/components/CodeFile.tsx
+++ packages/web/components/CodeFile.tsx
@@ -1,4 +1,4 @@
-import { useContext, useRef, useState, useEffect } from "react";
+import React, { useContext, useRef, useState, useEffect } from "react";
 import { css, CodeCard } from "@codeponder/ui";
 import { CommentProps, Comments } from "./commentUI";
 
@@ -132,11 +132,21 @@ const useHighlight = (lang: string, code: string) => {
 
 export const CodeFile: React.FC = () => {
   const { code, lang, owner, path, postId } = useContext(CodeFileContext);
+
   const highlightCode = useHighlight(lang, code || "");
   const variables = {
     path,
     postId,
   };
+
+  const [startTime] = useState(Date.now());
+  useEffect(() => {
+    console.log(
+      "time",
+      highlightCode.pending ? "lang pending" : "lang resolved",
+      Date.now() - startTime
+    );
+  });
 
   return (
     <FindCodeReviewQuestionsComponent variables={variables}>
@@ -149,23 +159,205 @@ export const CodeFile: React.FC = () => {
         const comments = getCommentsForFile(data, owner);
 
         return (
+          // <CodeCard
+          //   lang={lang}
+          //   selectedLines={SelectLines(data.findCodeReviewQuestions)}
+          //   onMouseOut={setIsHovered}
+          //   onMouseOver={setIsHovered}
+          // >
+          //   {highlightedCode.map((line, index) => (
+          //     <RenderLine
+          //       key={index}
+          //       comments={comments[index + 1]}
+          //       line={line}
+          //       lineNum={index + 1}
+          //     />
+          //   ))}
+          // </CodeCard>
           <CodeCard
             lang={lang}
             selectedLines={SelectLines(data.findCodeReviewQuestions)}
             onMouseOut={setIsHovered}
             onMouseOver={setIsHovered}
           >
-            {highlightedCode.map((line, index) => (
-              <RenderLine
-                key={index}
-                comments={comments[index + 1]}
-                line={line}
-                lineNum={index + 1}
-              />
-            ))}
+            <RenderCode comments={comments} highlightedCode={highlightedCode} />
           </CodeCard>
         );
       }}
     </FindCodeReviewQuestionsComponent>
   );
 };
+
+interface RenderCodeProps {
+  comments: Comments;
+  highlightedCode: string[];
+}
+
+// interface AddLinesProps {
+//   list: string[];
+//   setList: React.Dispatch<React.SetStateAction<string[]>>;
+//   highlightedCode: string[];
+// }
+
+type setFunction = React.Dispatch<React.SetStateAction<string[]>>;
+
+const addLinesAnim = (
+  list: string[],
+  setList: setFunction,
+  highlightedCode: string[]
+) => {
+  return {
+    requestHandle: 0,
+    start: function() {
+      if (!this.requestHandle && list.length < highlightedCode.length) {
+        this.requestHandle = window.requestAnimationFrame(
+          this.sample.bind(this)
+        );
+      }
+    },
+    stop: function() {
+      window.cancelAnimationFrame(this.requestHandle);
+      this.requestHandle = 0;
+    },
+    sample: function() {
+      if (list.length < highlightedCode.length) {
+        const amount = Math.min(
+          list.length + NUMBER_OF_LINE_TO_RENDER,
+          highlightedCode.length
+        );
+        const newList = highlightedCode.slice(0, amount);
+        console.log("newList", newList.length);
+        setList(newList);
+        this.requestHandle = window.requestAnimationFrame(
+          this.sample.bind(this)
+        );
+      }
+    },
+  };
+};
+
+export const RenderCode = React.memo(
+  ({ comments, highlightedCode }: RenderCodeProps) => {
+    const divRef = useRef<HTMLDivElement>(null);
+    console.log("RenderCode");
+    // useEffect(() => {
+    //   const start = Date.now();
+    //   const addLines = (items: string[]) => {
+    //     for (let index = 0; index < items.length; index++) {
+    //       const elm = document.createElement("div");
+    //       // elm.textContent = `line ${index + 1}`;
+    //       elm.innerHTML = items[index];
+    //       divRef.current!.appendChild(elm);
+    //     }
+    //   };
+    //   console.log("total time", Date.now() - start);
+
+    //   let requestHandle: number;
+    //   function request() {
+    //     const currentLength = divRef.current!.childNodes.length;
+    //     if (currentLength < highlightedCode.length) {
+    //       const amount = Math.min(
+    //         currentLength + NUMBER_OF_LINE_TO_RENDER,
+    //         highlightedCode.length
+    //       );
+    //       const newList = highlightedCode.slice(currentLength, amount);
+    //       console.log("newList", newList.length);
+    //       addLines(newList);
+    //       window.requestAnimationFrame(() => {
+    //         requestHandle = window.requestAnimationFrame(request);
+    //       });
+    //     }
+    //   }
+    //   requestHandle = window.requestAnimationFrame(request);
+    //   return () => {
+    //     window.cancelAnimationFrame(requestHandle);
+    //     requestHandle = 0;
+    //   };
+    // }, []);
+
+    useEffect(() => {
+      const elm = document.createElement("div");
+      // elm.textContent = `line ${index + 1}`;
+      elm.innerHTML = highlightedCode.join("\n");
+      divRef.current!.appendChild(elm);
+    }, []);
+
+    return <div ref={divRef} />;
+  }
+);
+
+const NUMBER_OF_LINE_TO_RENDER = 1000;
+export const RenderCode1 = React.memo(
+  ({ comments, highlightedCode }: RenderCodeProps) => {
+    const [list, setList] = useState(highlightedCode.slice(0, 200));
+
+    useEffect(() => {
+      // const linesAnim = addLinesAnim(list, setList, highlightedCode);
+      // linesAnim.start();
+      // return () => {
+      //   linesAnim.stop();
+      // };
+      let requestHandle: number;
+      function request() {
+        if (list.length < highlightedCode.length) {
+          const amount = Math.min(
+            list.length + NUMBER_OF_LINE_TO_RENDER,
+            highlightedCode.length
+          );
+          const newList = highlightedCode.slice(0, amount);
+          console.log("newList", newList.length);
+          setList(newList);
+          // requestHandle = window.requestAnimationFrame(request);
+        }
+      }
+      requestHandle = window.requestAnimationFrame(request);
+      return () => {
+        window.cancelAnimationFrame(requestHandle);
+        requestHandle = 0;
+      };
+
+      // const timer = setTimeout(() => {
+      //   if (list.length < highlightedCode.length) {
+      //     const amount = Math.min(
+      //       list.length + NUMBER_OF_LINE_TO_RENDER,
+      //       highlightedCode.length
+      //     );
+      //     setList(highlightedCode.slice(0, amount));
+      //   }
+      // });
+      // return () => {
+      //   clearTimeout(timer);
+      // };
+
+      // let deferred: any = {};
+      // new Promise(resolve => {
+      //   deferred.resolve = resolve;
+      //   if (list.length < highlightedCode.length) {
+      //     const amount = Math.min(
+      //       list.length + NUMBER_OF_LINE_TO_RENDER,
+      //       highlightedCode.length
+      //     );
+      //     setList(highlightedCode.slice(0, amount));
+      //   }
+      //   resolve();
+      // });
+      // return () => {
+      //   deferred.resolve();
+      //   deferred = null;
+      // };
+    });
+
+    return (
+      <>
+        {list.map((line, index) => (
+          <RenderLine
+            key={index}
+            comments={comments[index + 1]}
+            line={line}
+            lineNum={index + 1}
+          />
+        ))}
+      </>
+    );
+  }
+);
