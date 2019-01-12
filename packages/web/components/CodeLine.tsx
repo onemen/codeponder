b7cd03260fa8b64a5034508492ba8734df0e2f63
onemen <tabmix.onemen@gmail.com>
2019-01-12 16:40:00 +0200

    Wup, draft 8, move Discusion to new file

--- packages/web/components/CodeLine.tsx
+++ packages/web/components/CodeLine.tsx
@@ -1,9 +1,9 @@
-import { useCallback, useContext, useEffect, useState, useRef } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { Discussion } from "./Discussion";
 import { AddComment } from "./CommentSection";
-import { CommentProps, CommentBox } from "./commentUI";
-import { getScrollY } from "../utils/domScrollUtils";
+import { CommentProps } from "./commentUI";
 import { CodeFileContext } from "./CodeFileContext";
-import { styled } from "@codeponder/ui";
+import { getScrollY } from "../utils/domScrollUtils";
 
 interface RenderLineProps {
   comments: CommentProps[];
@@ -71,14 +71,6 @@ export const RenderLine: React.FC<RenderLineProps> = ({
     }
   }, []);
 
-  /*
-  TODO:
-  - move style to Pre style component or to Comments style component
-  - add new style to comments nav bar
-  - add actions to the nav bar
-  - add top nav bar
-    */
-
   return (
     <div key={lineNum} className="token-line">
       <span className="line-number" data-line-number={lineNum} />
@@ -87,7 +79,9 @@ export const RenderLine: React.FC<RenderLineProps> = ({
         dangerouslySetInnerHTML={{ __html: line }}
         onClick={onOpenEditor}
       />
-      <Discussion comments={commentsForRow} onOpenEditor={onOpenEditor} />
+      {commentsForRow.length && (
+        <Discussion comments={commentsForRow} onOpenEditor={onOpenEditor} />
+      )}
       {showEditor && (
         <AddComment
           comments={commentsForRow}
@@ -96,108 +90,5 @@ export const RenderLine: React.FC<RenderLineProps> = ({
         />
       )}
     </div>
-  );
-};
-
-const DiscussionNavBar = styled.div`
-  background-color: #f6f8fa;
-  border-bottom: 1px solid #e1e4e8;
-  padding: 10px;
-  display: flex;
-  justify-content: space-between;
-  align-items: baseline;
-
-  & .header-title {
-    font-weight: 400;
-    line-height: 1.125;
-    margin-bottom: 0;
-    word-wrap: break-word;
-  }
-
-  & .header-sub-title {
-    color: #a3aab1;
-    font-weight: 300;
-    letter-spacing: -1px;
-  }
-`;
-
-interface DiscussionProps {
-  comments: CommentProps[];
-  onOpenEditor: (props: any) => any;
-}
-
-const COLLAPSE = "Collapse this discussion";
-const EXPANDED = "Expanded this discussion";
-
-const lineNumbers = (comment: CommentProps) => {
-  const { startingLineNum, endingLineNum } = comment;
-  if (startingLineNum == endingLineNum) {
-    return `Line ${startingLineNum}`;
-  }
-  return `Lines ${startingLineNum} - ${endingLineNum}`;
-};
-
-const Discussion: React.FC<DiscussionProps> = ({ comments, onOpenEditor }) => {
-  const count = comments.length;
-  if (!count) {
-    return null;
-  }
-
-  const discussionRef = useRef<HTMLDivElement>(null);
-  const [showDiscussion, setShowDiscussion] = useState(false);
-
-  const onToggleDiscussion = useCallback(({ target: elm }: any) => {
-    if (elm.classList.contains("discussion-badge")) {
-      elm.classList.toggle("is-open");
-      if (discussionRef.current) {
-        discussionRef.current!.classList.remove("is-open");
-        // remove the component after the transition ends
-        setTimeout(() => {
-          setShowDiscussion(false);
-        }, 400);
-      } else {
-        setShowDiscussion(true);
-      }
-    }
-  }, []);
-
-  useEffect(
-    () => {
-      if (showDiscussion) {
-        discussionRef.current!.classList.add("is-open");
-      }
-    },
-    [showDiscussion]
-  );
-
-  return (
-    <>
-      <button
-        className="token-btn discussion-badge"
-        title={showDiscussion ? COLLAPSE : EXPANDED}
-        onClick={onToggleDiscussion}
-      >
-        <span className="badge-counter">{count}</span>
-        <span className="badge-icon">▾</span>
-      </button>
-      {showDiscussion && (
-        <div ref={discussionRef} className="discussion-container">
-          <div className="discussion-inner-box">
-            <DiscussionNavBar>
-              <h2 className="header-title">
-                <span className="discussion-title">Title placeholder</span>{" "}
-                <span className="header-sub-title">#???</span>
-              </h2>
-              <span className="header-sub-title">
-                {lineNumbers(comments[0])}
-              </span>
-            </DiscussionNavBar>
-            {comments.map((comment, key) => {
-              return <CommentBox {...{ ...comment, key, onOpenEditor }} />;
-            })}
-          </div>
-        </div>
-      )}
-    </>
   );
 };
