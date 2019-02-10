02521eae39da8d3abce27d7cbd4a3521f83b99c9
onemen <tabmix.onemen@gmail.com>
2019-02-10 21:00:45 +0200

    Remove obsolete types

--- packages/web/types/questionReplyTypes.ts
+++ packages/web/types/questionReplyTypes.ts
@@ -7,25 +7,3 @@ export interface EditorSubmitProps {
   submitted: boolean;
   response?: CodeReviewQuestionInfoFragment | QuestionReplyInfoFragment | void;
 }
-
-type KeysToExclude = {
-  replies: true;
-};
-
-export type QuestionInfo = Pick<
-  CodeReviewQuestionInfoFragment,
-  Exclude<keyof CodeReviewQuestionInfoFragment, keyof KeysToExclude>
->;
-
-export interface QuestionProps extends QuestionInfo {
-  isOwner: boolean;
-  newComment?: boolean;
-}
-
-interface ReplyProps extends QuestionReplyInfoFragment {
-  isOwner: boolean;
-  newComment?: boolean;
-  numReplies?: number; // not exist on reply but we use it in CommentCard
-}
-
-export type CommentProps = ReplyProps | QuestionProps;
